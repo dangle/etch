215698bd8cdf2e8e9783e157e2a4743341e0630b
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-18 05:47:57 -0600

    Add main.py.

---
+++ etch/main.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+from RPi import GPIO
+
+from etch import inputs, knob
+
+x = 0
+
+def clock():
+    global x
+    if x < 399:
+        x = x + 1
+    print(x)
+
+def counter():
+    global x
+    if x > 0:
+        x = x - 1
+    print(x)
+
+print('Listening for knob movements...')
+
+with inputs.Inputs() as i:
+    k = knob.Knob(17, 18, clockwise=clock, counterclockwise=counter)
+    i.register(k)
+    while 1:
+        sleep(0.5)
