372394f207d16ec6789950deb679d5ead55c757d
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-19 01:09:32 -0600

    Add in a second knob.

--- etch/main.py
+++ etch/main.py
@@ -7,11 +7,17 @@ from . import inputs, knob
 print('Listening for knob movements...')
 
 with inputs.Inputs() as i:
-    k = knob.Knob(17, 18)
-    i.register(k)
-    old_k = k.value
+    k1 = knob.Knob(17, 18)
+    i.register(k1)
+    old_k1 = k1.value
+    k2 = knob.Knob(7, 8)
+    i.register(k2)
+    old_k2 = k2.value
     while 1:
-        if k.value != old_k:
-            print(k.value)
-            old_k = k.value
+        if k1.value != old_k1:
+            print(f'LEFT KNOB {k1.value}')
+            old_k1 = k1.value
+        if k2.value != old_k2:
+            print(f'RIGHT KNOB {k2.value}')
+            old_k2 = k2.value
         sleep(0.01)
