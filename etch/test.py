0147e7292bbe8ef7da7267394aed4d412072304f
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-21 21:09:00 -0600

    Clear knob

--- etch/test.py
+++ etch/test.py
@@ -8,11 +8,9 @@ GPIO.setmode(GPIO.BCM)
 
 try:
     left = knob.Knob(19, 18, 12, 0, 399,
-                     changed=lambda v: set_pixel(x=v),
-                     clicked=lambda: print('LEFT CLICK'))
+                     changed=lambda v: set_pixel(x=v))
     right = knob.Knob(7, 16, 13, 0, 299,
-                      changed=lambda v: set_pixel(y=299 - v),
-                      clicked=lambda: print('RIGHT CLICK'))
+                      changed=lambda v: set_pixel(y=299 - v))
     while 1:
         time.sleep(0.01)
 finally:
