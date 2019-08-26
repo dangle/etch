a163189d3e7ca7905a9797c4c8020266381fa031
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-26 15:34:04 -0600

    Revert

--- etch/knob.py
+++ etch/knob.py
@@ -1,9 +1,14 @@
 import time
 
+from ctypes import c_uint8 as uint8
+from ctypes import c_uint16 as uint16
+
 from RPi import GPIO
 
 
 class Knob:
+
+    ENCODING_TABLE = (0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0)
 
     def __init__(self, clk, dt, sw=None, min_=None, max_=None, default=0, changed=None, clicked=None):
         self._value = default
@@ -14,13 +19,15 @@ class Knob:
         self._sw = sw
         self._clicked = clicked
         self._changed = changed
-        GPIO.setup(clk, GPIO.IN)
-        GPIO.setup(dt, GPIO.IN)
+        GPIO.setup(clk, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(dt, GPIO.IN, GPIO.PUD_UP)
         if sw:
-            GPIO.setup(sw, GPIO.IN)
+            GPIO.setup(sw, GPIO.IN, GPIO.PUD_UP)
         GPIO.add_event_detect(
             clk, GPIO.RISING, callback=lambda channel: self._rotated(),
-            bouncetime=2)
+            bouncetime=10)
+        self._prevNextCode = uint8(0)
+        self._storage = uint16(0)
 
     @property
     def value(self):
@@ -36,18 +43,12 @@ class Knob:
 
     def _rotated(self):
         time.sleep(0.002)
-        if self.clk == 1 and self.dt == 0:
-            while self.dt == 0:
-                pass
-            while self.dt == 1:
-                pass
+        if self.clk != self.dt:
             if self._max is None or (
                     self._max is not None and self._value < self._max):
                 self._value = self._value + 1
                 self._changed(self._value)
-        elif self.clk == 1 and self.dt == 1:
-            while self.dt == 1:
-                pass
+        elif:
             if self._min is None or (
                     self._min is not None and self._value > self._min):
                 self._value = self._value - 1
