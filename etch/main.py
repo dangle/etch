6d982c2f2615fb3ed118379086c338d30630c02f
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-30 20:15:08 -0600

    Know left and right.

--- etch/main.py
+++ etch/main.py
@@ -10,7 +10,7 @@ try:
     left = knob.Knob(17, 6, 13, 0, 399,
                      updated=lambda v: print(f'LEFT {v}'),
                      pressed=lambda: print('LEFT PRESSED'),
-                     released=lambda: print('RIGHT RELEASED')
+                     released=lambda: print('LEFT RELEASED')
                     )
     right = knob.Knob(16, 5, 12, 0, 299,
                       updated=lambda v: print(f'RIGHT {v}'),
