e747285ec4f7392be7787140269d299ae8f54079
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-30 23:49:44 -0600

    Longer long press

--- etch/knob.py
+++ etch/knob.py
@@ -70,7 +70,7 @@ class Knob:
 
     @property
     def is_long_pressed(self):
-        return self._sw and self.pushed_duration.seconds > 0.5
+        return self._sw and self.pushed_duration.seconds > 1
 
     @property
     def pushed_duration(self):
