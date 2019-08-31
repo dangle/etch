069ca27a9b3f105fda415359f747956b184e2424
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-30 20:14:11 -0600

    Add callbacks for pressed and released.

--- etch/main.py
+++ etch/main.py
@@ -9,10 +9,14 @@ GPIO.setmode(GPIO.BCM)
 try:
     left = knob.Knob(17, 6, 13, 0, 399,
                      updated=lambda v: print(f'LEFT {v}'),
-                     pressed=lambda: print('LEFT CLICK'))
+                     pressed=lambda: print('LEFT PRESSED'),
+                     released=lambda: print('RIGHT RELEASED')
+                    )
     right = knob.Knob(16, 5, 12, 0, 299,
                       updated=lambda v: print(f'RIGHT {v}'),
-                      pressed=lambda: print('RIGHT CLICK'))
+                      pressed=lambda: print('RIGHT PRESSED').
+                      released=lambda: print('RIGHT RELEASED')
+                    )
     while 1:
         time.sleep(0.01)
 finally:
