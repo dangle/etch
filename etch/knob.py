dd60d008619d5f14d80ac2d4d7ad17a405aca1e2
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-30 20:11:22 -0600

    Better logic for button presses.

--- etch/knob.py
+++ etch/knob.py
@@ -5,15 +5,17 @@ from RPi import GPIO
 
 class Knob:
 
-    def __init__(self, clk, dt, sw=None, min_=None, max_=None, default=0, changed=None, clicked=None):
+    def __init__(self, clk, dt, sw=None, min_=None, max_=None, default=0,
+                 updated=None, pressed=None, released=None):
         self._value = default
         self._min = min_
         self._max = max_
         self._clk = clk
         self._dt = dt
         self._sw = sw
-        self._clicked = clicked or (lambda v: None)
-        self._changed = changed or (lambda v: None)
+        self._updated = updated or (lambda v: None)
+        self._pressed = pressed or (lambda v: None)
+        self._released = released or (lambda v: None)
         self._setup_rotate()
         self._setup_click()
 
@@ -31,6 +33,12 @@ class Knob:
                 self._sw, GPIO.BOTH, callback=lambda channel: self._clicked(),
                 bouncetime=20)
 
+    def _clicked(self):
+        if self.sw:
+            self._pressed()
+        else:
+            self._released()
+
     @property
     def value(self):
         return self._value
@@ -43,14 +51,19 @@ class Knob:
     def dt(self):
         return GPIO.input(self._dt)
 
+    @property
+    def sw(self):
+        if self._sw:
+            return GPIO.input(self._sw)
+
     def _rotated(self):
         if self.clk == self.dt:
             if self._max is None or (
                     self._max is not None and self._value < self._max):
                 self._value = self._value + 1
-                self._changed(self._value)
+                self._updated(self._value)
         else:
             if self._min is None or (
                     self._min is not None and self._value > self._min):
                 self._value = self._value - 1
-                self._changed(self._value)
+                self._updated(self._value)
--- etch/main.py
+++ etch/main.py
@@ -8,11 +8,11 @@ GPIO.setmode(GPIO.BCM)
 
 try:
     left = knob.Knob(17, 6, 13, 0, 399,
-                     changed=lambda v: print(f'LEFT {v}'),
-                     clicked=lambda: print('LEFT CLICK'))
+                     updated=lambda v: print(f'LEFT {v}'),
+                     pressed=lambda: print('LEFT CLICK'))
     right = knob.Knob(16, 5, 12, 0, 299,
-                      changed=lambda v: print(f'RIGHT {v}'),
-                      clicked=lambda: print('RIGHT CLICK'))
+                      updated=lambda v: print(f'RIGHT {v}'),
+                      pressed=lambda: print('RIGHT CLICK'))
     while 1:
         time.sleep(0.01)
 finally:
