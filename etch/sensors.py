64cffa46c247961b9371c87d4dd2bbed7f9d4133
Melissa Nuño <dangle@users.noreply.github.com>
2019-08-31 20:49:39 -0600

    Debug

--- etch/sensors.py
+++ etch/sensors.py
@@ -22,9 +22,9 @@ class Sensor:
     def __init__(self, on_shake=None):
         self._on_shake = on_shake or DO_NOTHING
         self._sensor = mpu6050(self._I2C_ADDRESS)
-        self._offset = mean(1
+        self._offset = mean(self._calc_accel(x, y, z)
                             for _ in range(self._OFFSET_SAMPLES)
-                            for x, y, z in self.accelerometer)
+                            for x, y, z in [0.0,0]])
         self._setup_shaking()
 
     def configure(self, on_shake=NOT_SUPPLIED):
